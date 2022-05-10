003cf7fb39f2b7863faef60f2fb3b0e3661bcd6e
lei zp <leizhipeng@youloft.com>
2022-05-10 17:40:11 +0800

    更新

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
+use origin_type::origin_type::cs;
+
 // mod helloworld;
-mod origin_type;
+// mod origin_type;
+
+mod custom_type;
 
 fn main() {
-    origin_type::origin_type::cs();
+    // origin_type::origin_type::cs();
+    custom_type::custom_type::cs();
 }
--- src/origin_type/origin_type.rs
+++ src/origin_type/origin_type.rs
@@ -1,4 +1,5 @@
 use std::fmt;
+use std::mem;
 
 pub fn cs() {
     let long_tuple = (1i32, 2u32);
@@ -23,4 +24,37 @@ pub fn cs() {
     // } 
 
     // transpose(matrix)
+
+    // 切片
+    // 定长数组（类型标记是多余的）
+    let xs: [i32; 5] = [1, 2, 3, 4, 5];
+
+    // 所有元素可以初始化成相同的值
+    let ys: [i32; 500] = [0; 500];
+
+    // 下标从 0 开始
+    println!("first element of the array: {}", xs[0]);
+    println!("second element of the array: {}", xs[1]);
+    // `len` 返回数组的大小
+    println!("array size: {}", xs.len());
+
+    // 数组是在栈中分配的
+    println!("array occupies {} bytes", mem::size_of_val(&xs));
+
+    // 数组可以自动被借用成为 slice
+    println!("borrow the whole array as a slice");
+    analyze_slice(&xs[1 .. 4]);
+
+    // slice 可以指向数组的一部分
+    println!("borrow a section of the array as a slice");
+    analyze_slice(&ys[1 .. 4]);
+
+    // 越界的下标会引发致命错误（panic）
+    // println!("{}", xs[5]);
+}
+
+fn analyze_slice(slice: &[i32]) {
+    println!("first element of the slice: {}", slice[0]);
+    println!("the slice has {} elements", slice.len());
+    println!("the slice has {:?} elements", slice);
 }
